fbff0e8b0dc5f3139ec8c25bc0a5d008e7f55ca6
EnriqueBC <40406742@live.napier.ac.uk>
2020-11-18 16:29:16 +0000

    String Reverse Finished

---
+++ StringReverse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CodingChallenge.ReversingString;
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varia = StringUtilities.Reverse("Hello World!");
+            Console.WriteLine(varia);
+        }
+    }
+}
---
+++ src/ReversingString/StringUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodingChallenge.ReversingString
+{
+    public class StringUtilities
+    {
+        public static string Reverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            s = new string(arr);
+            return s;
+
+            throw new NotImplementedException();
+        }
+    }
+}
